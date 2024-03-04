40f1e8e88f9f7c5079eccc84d105626f6d1ae751
David Kipperman <kippermand@gmail.com>
2024-03-04 12:43:49 -0500

    Push API (No Docker) (#6)
    * remove dockerfile
    
    * Update launchSettings.json
    
    remove docker support
    
    * Update launchSettings.json
    
    * Added Azure Resource Manager provider and resources in Terraform
    
    This commit introduces a new Azure Resource Manager (ARM) provider configuration in Terraform, enabling interaction with Azure resources through ARM APIs. It defines several variables with default values for resource group, app service plan, application insights, and API service.
    
    The code creates a new Azure resource group, App Service Plan, Application Insights resource, and a Linux Web App with specified parameters. The Linux Web App configuration is set to always be off, HTTPS only, and includes the Application Insights instrumentation key in the app settings.
    
    * `Update GitHub Actions workflow for dev deployment`
    
    Updated the `deploy-to-dev.yml` file to configure a GitHub Actions workflow for deploying an application to a development server. The workflow is triggered on a push event to the 'develop' and 'feature/*' branches. It consists of two jobs: `terraform` and `deploy-api`.
    
    The `terraform` job sets up the environment variables for Terraform and runs several steps including code checkout, Terraform setup, initialization, validation, and application. Azure credentials are provided as secrets for the Terraform apply step.
    
    The `deploy-api` job sets up the environment variables for the .NET application and runs steps including code checkout, .NET setup, dependency restoration, project build, project publish, Azure login, and API deployment to Azure. Azure credentials are provided as secrets for the Azure login step.
    
    * fix name
    
    * update packages

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+# terraform/main.tf
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+    default = "nebamgmt-rg-test"
+}
+
+resource "azurerm_resource_group" "nebamgmt-rg" {
+  name     = var.resource_group_name
+  location = "East US"
+}
+
+variable "app_service_plan_name" {
+    default = "nebamgmt-asp-test"
+}
+
+variable "app_service_plan_sku_name" {
+    default = "F1"
+}
+
+resource "azurerm_service_plan" "nebamgmt-asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.nebamgmt-rg.location
+  resource_group_name = azurerm_resource_group.nebamgmt-rg.name
+  os_type = "Linux"
+  sku_name = var.app_service_plan_sku_name
+}
+
+variable "app_insights_name"{
+    default = "nebamgmt-ai-test"
+}
+
+resource "azurerm_application_insights" "nebamgmt-ai" {
+  name                = var.app_insights_name
+  location            = azurerm_resource_group.nebamgmt-rg.location
+  resource_group_name = azurerm_resource_group.nebamgmt-rg.name
+  application_type    = "web"
+}
+
+variable "api_service_name" {
+    default = "nebamgmt-api-test"
+}
+
+resource "azurerm_linux_web_app" "nebamgmt-api"{
+    name = var.api_service_name
+    location = azurerm_resource_group.nebamgmt-rg.location
+    resource_group_name = azurerm_resource_group.nebamgmt-rg.name
+    service_plan_id = azurerm_service_plan.nebamgmt-asp.id
+
+    site_config {
+        always_on = false
+    }
+
+    https_only = true
+
+    app_settings = {
+        "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.nebamgmt-ai.instrumentation_key
+    }
+}
