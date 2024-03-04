8ed0a5afa67e733badfa393a7006fe7d5fb3db70
Dave Kipperman <kippermand@gmail.com>
2024-03-04 18:42:03 -0500

    "Added MudBlazor package and configured its services"
    Added the `MudBlazor` package version `6.17.0` to the `Directory.Packages.props` file and updated the `Neba.UI.csproj` file to include a reference to this package. The `_Imports.razor` and `Program.cs` files were updated to include the `MudBlazor` and `MudBlazor.Services` namespaces. In `Program.cs`, the `AddMudServices` method was called on the `Services` property of the `builder` object to add MudBlazor services to the application and the configuration for the Snackbar component was set.

--- src/Neba.UI/Program.cs
+++ src/Neba.UI/Program.cs
@@ -1,6 +1,20 @@
+using MudBlazor;
+using MudBlazor.Services;
 using Neba.UI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopEnd;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
