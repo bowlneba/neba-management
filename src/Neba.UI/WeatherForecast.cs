f25d5945156d9f4a78efd33bd726fe0a5b78bb18
Dave Kipperman <kippermand@gmail.com>
2024-03-05 12:48:06 -0500

    Update Weather.razor to use IWeatherService and handle errors
    The `Weather.razor` file has been significantly updated. It now uses an injected `IWeatherService` to fetch weather data, replacing the previous method of generating random data. The `OnInitializedAsync` method has been updated to use this service and handle any errors that may occur. An `errorMessage` variable has been added to handle these errors, and an error message is now displayed if there is an issue fetching the weather data. The `forecasts` variable is now set with the value returned from the `IWeatherService`. The `WeatherForecast` class has been updated for serialization and deserialization, with the `TemperatureF` property now being settable. The `System.Security.Cryptography` namespace is no longer used. Lastly, the `StreamRendering` attribute has been added to potentially improve load times.

--- src/Neba.UI/WeatherForecast.cs
+++ src/Neba.UI/WeatherForecast.cs
@@ -1,9 +1,18 @@
-﻿namespace Neba.UI;
+﻿using System.Text.Json.Serialization;
+
+namespace Neba.UI;
 
 public class WeatherForecast
 {
+    [JsonPropertyName("date")]
     public DateOnly Date { get; set; }
+
+    [JsonPropertyName("temperatureC")]
     public int TemperatureC { get; set; }
+
+    [JsonPropertyName("summary")]
     public string? Summary { get; set; }
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    [JsonPropertyName("temperatureF")]
+    public int TemperatureF { get; set; }
 }
