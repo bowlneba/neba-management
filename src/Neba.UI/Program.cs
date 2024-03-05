ceddea488fa8348d1f2980c3f08a5df9bbdfcc88
David Kipperman <kippermand@gmail.com>
2024-03-05 09:12:41 -0500

    Create UI Project (#9)
    * Update project structure, add Neba.UI project, and update styles
    
    Updated the project type for "Neba.Api.UnitTests" in the solution file. Added a new project "Neba.UI" to the solution file and created a corresponding project file "Neba.UI.csproj" with target framework set to "net8.0". Also, created a new Program.cs file for the "Neba.UI" project and added new Razor component files and corresponding CSS files. Updated the application URLs for both "http" and "https" profiles in the launchSettings.json file and removed the "IIS Express" profile. Added new settings for logging to appsettings files. Updated the `app.css` file with new styling rules and added a new `favicon.png` file.
    
    * "Added MudBlazor package and configured its services"
    
    Added the `MudBlazor` package version `6.17.0` to the `Directory.Packages.props` file and updated the `Neba.UI.csproj` file to include a reference to this package. The `_Imports.razor` and `Program.cs` files were updated to include the `MudBlazor` and `MudBlazor.Services` namespaces. In `Program.cs`, the `AddMudServices` method was called on the `Services` property of the `builder` object to add MudBlazor services to the application and the configuration for the Snackbar component was set.
    
    * remove bootstrap
    
    * Updated project type and added NebaThemeProvider
    
    Updated the project type of "Neba.UI" in "Neba.sln" by replacing the old project type GUID with a new one. Added a new file "NebaThemeProvider.razor" with features including the use of MudBlazor.Utilities namespace, a CascadingValue component, a MudThemeProvider component, a boolean parameter "IsDarkMode", a static readonly MudTheme named "Neba", a method "SetDarkMode", a private variable "_mudThemeProvider", and an async method "IsSystemDarkMode".
    
    * Updated layout and removed unused components
    
    Updated `App.razor` to remove old stylesheets, change favicon, import Roboto font, and add MudBlazor stylesheet and script. `MainLayout.razor` was significantly redesigned using MudBlazor components, including a new AppBar, dark mode switch, and navigation drawer. Added `MudDialogProvider` and `MudSnackbarProvider`. Removed link to `counter` page in `NavMenu.razor` and deleted `Counter.razor` page. Removed `FocusOnNavigate` component from `Routes.razor`.
    
    * Refactor navigation menu and migrate to MudNavMenu
    
    In this commit, the navigation menu was significantly refactored. The `MainLayout.razor` file was updated to replace the comment `<!-- Main Nav Menu -->` with the `<MainNavMenu />` component. The `NavMenu.razor` and `NavMenu.razor.css` files were completely cleared out, removing the old navigation menu and its associated styles. A new file `MainNavMenu.razor` was created, implementing a `MudNavMenu` component with links to the home and weather pages, along with their associated icons.

---
+++ src/Neba.UI/Program.cs
@@ -0,0 +1,41 @@
+using MudBlazor;
+using MudBlazor.Services;
+using Neba.UI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopEnd;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
