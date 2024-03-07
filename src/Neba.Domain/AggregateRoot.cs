48f83c4f44abeea8a02584f380f1141c2c3e7887
Dave Kipperman <kippermand@gmail.com>
2024-03-07 10:03:20 -0500

    Added AggregateRoot abstract class and IDomainEvent interface
    In this commit, a new abstract class `AggregateRoot<TId>` has been added to the `AggregateRoot.cs` file. This class inherits from `Entity<TId>` and implements the `IAggregateRoot` interface. It includes a private list of `IDomainEvent` objects, a constructor, and two methods: `PopDomainEvents` and `AddDomainEvent`. An `IAggregateRoot` interface has also been added, which declares the `PopDomainEvents` method.
    
    Additionally, a new interface `IDomainEvent` has been added in the `IDomainEvent.cs` file.
    
    The `AggregateRootTests.cs` file has been updated to include a new class `AggregateRootTests` with two test methods: `AggregateRoot_IsAnEntity` and `PopDomainEvents_WhenCalled_ReturnsAndClearsDomainEvents`. A `TestAggregateRoot` class has also been added for testing purposes.

---
+++ src/Neba.Domain/AggregateRoot.cs
@@ -0,0 +1,29 @@
+﻿namespace Neba.Domain;
+
+public abstract class AggregateRoot<TId>
+    : Entity<TId>, IAggregateRoot
+where TId : struct, IEquatable<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    protected AggregateRoot(TId id)
+        : base(id)
+    {
+    }
+
+    public IReadOnlyCollection<IDomainEvent> PopDomainEvents()
+    {
+        var domainEvents = _domainEvents.ToList();
+        _domainEvents.Clear();
+
+        return domainEvents;
+    }
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+        => _domainEvents.Add(domainEvent);
+}
+
+public interface IAggregateRoot
+{
+    IReadOnlyCollection<IDomainEvent> PopDomainEvents();
+}
