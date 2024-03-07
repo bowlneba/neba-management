42e7d5afa81f4125ed61457899d015f5373e46df
Dave Kipperman <kippermand@gmail.com>
2024-03-07 09:53:44 -0500

    Add Ardalis.GuardClauses, Neba.Domain, and unit tests
    Added the `Ardalis.GuardClauses` package and two new projects, `Neba.Domain` and `Neba.Domain.UnitTests`, to the solution. The `Neba.Domain` project includes an abstract `Entity` class that uses the `Ardalis.GuardClauses` package to guard against default values. The `Neba.Domain.UnitTests` project includes tests for the `Entity` class. Both new projects target .NET 8.0. Removed the `Calculator` and `CalculatorTests` classes from the `Neba.Api` and `Neba.Api.UnitTests` namespaces respectively.

---
+++ tests/Neba.Domain.UnitTests/EntityTests.cs
@@ -0,0 +1,86 @@
+﻿namespace Neba.Domain.UnitTests;
+
+public class EntityTests
+{
+    [Fact]
+    public void WhenConstructedWithDefaultValue_ExceptionIsThrown()
+    {
+        var exception = Record.Exception(() => new TestEntity(default));
+
+        exception.Should().NotBeNull();
+        exception.Should().BeOfType<ArgumentException>();
+    }
+
+    [Fact]
+    public void Equals_WhenOtherIsNull_ReturnsFalse()
+    {
+        var entity = new TestEntity(1);
+
+        var result = entity.Equals(null);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_WhenOtherIsNotEntity_ReturnsFalse()
+    {
+        var entity = new TestEntity(1);
+
+        var result = entity.Equals(new object());
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_WhenOtherIsEntityAndIdsAreNotEqual_ReturnsFalse()
+    {
+        var entity = new TestEntity(1);
+        var other = new TestEntity(2);
+
+        var result = entity.Equals(other);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_WhenOtherIsEntityAndIdsAreEqual_ReturnsTrue()
+    {
+        var entity = new TestEntity(1);
+        var other = new TestEntity(1);
+
+        var result = entity.Equals(other);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_WhenOtherIsEntityAndIdsAreEqualAndOtherValueIsNot_ReturnsTrue()
+    {
+        var entity = new TestEntity(1) { OtherValue = 1 };
+        var other = new TestEntity(1) { OtherValue = 2 };
+
+        var result = entity.Equals(other);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsIdValue()
+    {
+        var entity = new TestEntity(1);
+
+        var result = entity.GetHashCode();
+
+        result.Should().Be(1.GetHashCode());
+    
+    }
+}
+
+internal sealed class TestEntity : Entity<int>
+{
+    public TestEntity(int id) : base(id)
+    {
+    }
+
+    public int OtherValue { get; init; }
+}
