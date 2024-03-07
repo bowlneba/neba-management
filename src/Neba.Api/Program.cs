31ba1def54891a3d15ee57f465c612b2036b47c0
Dave Kipperman <kippermand@gmail.com>
2024-03-07 10:47:13 -0500

    Added Neba.Application reference and shared services
    Added a reference to the `Neba.Application` project in the `Neba.Api` project file, allowing the use of classes and interfaces from `Neba.Application`. The `Neba.Application` namespace was also added to the `Program.cs` file for easier access to these classes and interfaces. A call to `builder.Services.AddSharedApplicationServices();` was added to `Program.cs` to add shared services to the dependency injection container. A new file `ApplicationDependencyInjection.cs` was created, containing a static class and method for adding services to the dependency injection container using the `MediatR` library and FluentValidation.

--- src/Neba.Api/Program.cs
+++ src/Neba.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using Neba.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSharedApplicationServices();
 
 var app = builder.Build();
 
