f6310a0796ff0c3b0701511e1b0d7014ef0b0b68
Dave Kipperman <kippermand@gmail.com>
2024-03-07 16:09:28 -0500

    Add SQL Server package and KeyValueObserver class
    This commit includes the addition of the `Microsoft.EntityFrameworkCore.SqlServer` package version `8.0.2` to the `Directory.Packages.props` and `Neba.Infrastructure.csproj` files for SQL Server database operations. A new class `KeyValueObserver` has been added to the `KeyValueObserver.cs` file, which implements the `IObserver<KeyValuePair<string, object?>>` interface for observing key-value pairs. The `KeyValueObserverLogMessages` class has also been added to the same file, providing extension methods for the `ILogger<KeyValueObserver>` interface to log error and trace messages.

---
+++ src/Neba.Infrastructure/Diagnostics/KeyValueObserver.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Neba.Infrastructure.Diagnostics;
+
+internal sealed class KeyValueObserver
+    : IObserver<KeyValuePair<string, object?>>
+{
+    private readonly ILogger<KeyValueObserver> _logger;
+
+    public KeyValueObserver(ILogger<KeyValueObserver> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnCompleted()
+    {
+    }
+
+    public void OnError(Exception error)
+        => _logger.ObserverError(error);
+
+    public void OnNext(KeyValuePair<string, object?> value)
+    {
+        if (value.Key == CoreEventId.ContextInitialized.Name)
+        {
+            var contextInitializedEventData = value.Value as ContextInitializedEventData;
+            _logger.ContextInitialized(contextInitializedEventData?.Context.GetType().Name);
+
+            return;
+        }
+
+        if (value.Key != RelationalEventId.ConnectionOpening.Name)
+        {
+            return;
+        }
+
+        var connectionEventData = value.Value as ConnectionEventData;
+        _logger.ConnectionOpening(connectionEventData?.Connection.DataSource);
+    }
+}
+
+internal static partial class KeyValueObserverLogMessages
+{
+    [LoggerMessage(Level = LogLevel.Error, Message = "An error occurred in the observer")]
+    public static partial void ObserverError(this ILogger<KeyValueObserver> logger, Exception ex);
+
+    [LoggerMessage(Level = LogLevel.Trace, Message = "Context {ContextType} has been initialized")]
+    public static partial void ContextInitialized(this ILogger<KeyValueObserver> logger, string? contextType);
+
+    [LoggerMessage(Level = LogLevel.Trace, Message = "Connection to database {DatabaseName} is being opened")]
+    public static partial void ConnectionOpening(this ILogger<KeyValueObserver> logger, string? databaseName);
+}
