4d03750c2f07ec763245ca57db8dc0bba99197ab
Dave Kipperman <kippermand@gmail.com>
2024-03-07 10:10:26 -0500

    Added ICommandHandler interfaces in ICommandHandler.cs
    In the `ICommandHandler.cs` file, two new `using` directives, `using ErrorOr;` and `using MediatR;`, have been added to include the `ErrorOr` and `MediatR` namespaces. A new namespace `Neba.Application.Messaging` has been declared. Within this namespace, two new public interfaces, `ICommandHandler<in TCommand>` and `ICommandHandler<in TCommand, TResult>`, have been declared. These interfaces inherit from `IRequestHandler<TCommand, ErrorOr<Success>>` and `IRequestHandler<TCommand, ErrorOr<TResult>>` respectively. Type constraints have been added to ensure `TCommand` implements the `ICommand` or `ICommand<TResult>` interface.

---
+++ src/Neba.Application/Messaging/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Neba.Application.Messaging;
+
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, ErrorOr<Success>>
+    where TCommand : ICommand;
+
+public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, ErrorOr<TResult>>
+    where TCommand : ICommand<TResult>;
