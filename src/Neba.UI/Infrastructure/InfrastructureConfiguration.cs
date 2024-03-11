6e51c45469f22636065c86ce6c31c7e0a8f99eb6
David Kipperman <kippermand@gmail.com>
2024-03-11 17:26:36 -0400

    Remove unused code related to feature management

--- src/Neba.UI/Infrastructure/InfrastructureConfiguration.cs
+++ src/Neba.UI/Infrastructure/InfrastructureConfiguration.cs
@@ -21,9 +21,7 @@ internal static class InfrastructureConfiguration
 #if DEBUG
 
         builder.Configuration.AddJsonFile("appsettings.Development.json", false, true);
-
-        builder.Services.AddFeatureManagement(builder.Configuration.GetSection("FeatureManagement"));
-
+        
 #else
 
 #endif
