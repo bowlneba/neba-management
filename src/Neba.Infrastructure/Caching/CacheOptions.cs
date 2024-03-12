dbd0b318a019ed738129fb0257487ff6b36fa684
David Kipperman <kippermand@gmail.com>
2024-03-12 14:43:02 -0400

    cache serviced

---
+++ src/Neba.Infrastructure/Caching/CacheOptions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Neba.Infrastructure.Caching;
+
+internal static class CacheOptions
+{
+    private static DistributedCacheEntryOptions DefaultExpiration
+        => new()
+        {
+#if DEBUG
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30)
+#else
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(12)
+#endif
+        };
+
+    public static DistributedCacheEntryOptions Create(TimeSpan? expiration)
+    {
+        return expiration is not null
+            ? new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiration.Value
+            }
+            : DefaultExpiration;
+    }
+}
