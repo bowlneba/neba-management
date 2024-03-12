c0d4437d2d81fb7c96e1d12c3f66a51676075135
David Kipperman <kippermand@gmail.com>
2024-03-12 14:05:05 -0400

    add ICacheService

---
+++ src/Neba.Application/Caching/ICacheService.cs
@@ -0,0 +1,10 @@
+namespace Neba.Application.Caching;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken);
+
+    Task SetAsync<T>(string key, T value, TimeSpan? expiration, CancellationToken cancellationToken);
+
+    Task RemoveAsync(string key, CancellationToken cancellationToken);
+}
