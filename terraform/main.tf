1371d3a63c26a27e358a2c15f2c4f33e02b489df
David Kipperman <kippermand@gmail.com>
2024-03-14 10:54:24 -0400

    Update read_scale to false in azurerm_mssql_database resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -381,7 +381,7 @@ resource "azurerm_mssql_database" "nebamgmt-sql-database"{
   collation = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb = 2
-  read_scale = true
+  read_scale = false
   sku_name = "S0"
   zone_redundant = false
 
