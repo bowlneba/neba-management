aec8b399de8e53aea7d0500b91613d8456154774
David Kipperman <kippermand@gmail.com>
2024-03-20 09:59:38 -0400

    Subject: Added Encryption class and corresponding unit tests
    Body:
    This commit introduces a new `Encryption` class in the `Neba.UI.Services` namespace, implementing the `IEncryption` interface. The class uses RSA encryption with a 2048-bit key, generated from a `JsonWebKey` object. The `Encrypt` method returns an encrypted string, or an empty string for null or empty input.
    
    Additionally, a new `EncryptionTests` class has been added to the `Neba.UI.UnitTests.Services` namespace, providing unit tests for the `Encryption` class. The tests validate the encryption process, ensuring different outputs for the same input, and correct handling of null or empty strings.
    
    Minor formatting changes have also been made in `Neba.UI.UnitTests.csproj`, specifically adjusting the closing tag of the `FrameworkReference` element.

---
+++ tests/Neba.UI.UnitTests/Services/EncryptionTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+using Azure.Security.KeyVault.Keys;
+using Neba.UI.Services;
+
+namespace Neba.UI.UnitTests.Services;
+
+public sealed class EncryptionTests
+{
+    private readonly Encryption _encryption;
+
+    public EncryptionTests()
+    {
+        var rsa = RSA.Create(2048);
+        var jsonWebKey = new JsonWebKey(rsa);
+
+        _encryption = new Encryption(jsonWebKey);
+    }
+
+    [Fact]
+    public void Encrypt_WhenDoneMultipleTimesOnTheSameValue_ShouldReturnDifferentValues()
+    {
+        // Arrange
+        var data = "Hello, World!";
+
+        // Act
+        var encryptedData1 = _encryption.Encrypt(data);
+        var encryptedData2 = _encryption.Encrypt(data);
+
+        // Assert
+        encryptedData2.Should().NotBe(encryptedData1);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Encrypt_WhenDataIsNullOrEmpty_ShouldReturnAnEmptyString(string? data)
+    {
+        // Act
+        var result = _encryption.Encrypt(data);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("  ")]
+    public void Encrypt_WhenDataIsSpaces_ShouldReturnAnEncryptedValue(string data)
+    {
+        // Act
+        var result = _encryption.Encrypt(data);
+
+        // Assert
+        result.Should().NotBeEmpty();
+    }
+}
