e72d6d78ff94e13cbd8fccaf9250cc3d58acb046
David Kipperman <kippermand@gmail.com>
2024-03-20 09:27:42 -0400

    Updated package and added IEncryption interface
    Updated the `Microsoft.FeatureManagement` package reference in the `Neba.Application.csproj` file. Added a new namespace `Neba.Application.Security` and a new interface `IEncryption` in the `IEncryption.cs` file. The `IEncryption` interface includes two asynchronous methods: `EncryptAsync` and `DecryptAsync`, both accepting a nullable string `data` and a `CancellationToken` `cancellationToken`.

---
+++ src/Neba.Application/Security/IEncryption.cs
@@ -0,0 +1,8 @@
+﻿namespace Neba.Application.Security;
+
+public interface IEncryption
+{
+    Task<string> EncryptAsync(string? data, CancellationToken cancellationToken);
+
+    Task<string> DecryptAsync(string? data, CancellationToken cancellationToken);
+}
