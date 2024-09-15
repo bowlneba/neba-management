a8fd0d81bda4493afc4846f5deb20431a40b21a1
David Kipperman <kippermand@gmail.com>
2024-09-14 22:45:22 -0400

    Add WebApi.Endpoints project and FastEndpoints integration
    - Added properties in Directory.Build.props for documentation and build acceleration.
    - Updated Directory.Packages.props with a new package version for FastEndpoints.
    - Included WebApi.Endpoints project in Neba.sln and added solution items.
    - Introduced AssemblyMarker class for endpoint assembly marking.
    - Added GetWeatherForecast endpoint using FastEndpoints.
    - Moved and updated WeatherForecast record with additional features.
    - Created WebApi.Endpoints.csproj with FastEndpoints reference.
    - Updated Program.cs to use FastEndpoints and removed inline endpoint.
    - Added project reference to WebApi.Endpoints in WebApi.csproj.

---
+++ src/WebApi.Endpoints/AssemblyMarker.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Neba.WebApi.Endpoints;
+
+/// <summary>
+/// Provides a marker for the assembly containing the endpoints.
+/// </summary>
+public static class AssemblyMarker
+{
+    /// <summary>
+    /// Gets the assembly that contains the endpoint declarations.
+    /// </summary>
+    public static Assembly Assembly
+        => typeof(WeatherForecast).Assembly;
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,11 +1,14 @@
-using System.Security.Cryptography;
-using Neba.WebApi;
+using FastEndpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Specify the assembly containing your endpoints
+builder.Services.AddFastEndpoints(options
+    => options.Assemblies = [Neba.WebApi.Endpoints.AssemblyMarker.Assembly]);
 
 var app = builder.Build();
 
@@ -17,23 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            RandomNumberGenerator.GetInt32(-20, 55),
-            summaries[RandomNumberGenerator.GetInt32(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseFastEndpoints();
 
 await app.RunAsync();
