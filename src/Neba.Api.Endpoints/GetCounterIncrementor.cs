2f219282c90d29cd8522aa10442f92e5144b1abd
Dave Kipperman <kippermand@gmail.com>
2024-09-16 13:01:31 -0400

    Add new GET endpoint for counter incrementor
    Introduce `GetCounterIncrementor` endpoint in `Neba.Api.Endpoints` using FastEndpoints. The endpoint responds to GET requests at the "counter" route, supports version 1, and allows anonymous access. It generates a random integer between 2 and 10 after a 5ms delay and returns it as an `Ok` result. Imported necessary namespaces and added pragma to disable CA1812 warning.

---
+++ src/Neba.Api.Endpoints/GetCounterIncrementor.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Neba.Api.Endpoints;
+
+#pragma warning disable CA1812
+internal sealed class GetCounterIncrementor
+    : EndpointWithoutRequest<Results<Ok<int>, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Get("counter");
+        Version(1);
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<int>, ProblemDetails>> ExecuteAsync(CancellationToken ct)
+    {
+        await Task.Delay(5, ct);
+
+        var incrementor = RandomNumberGenerator.GetInt32(2, 10);
+
+        return TypedResults.Ok(incrementor);
+    }
+}
