d09ca453ebc47f07b272f42e4b38190b52a0d5a4
Dave Kipperman <kippermand@gmail.com>
2024-09-19 13:33:53 -0400

    Update Azure backend config in main.tf
    Changed resource_group_name from "bowlneba" to "infrastructure".
    Updated storage_account_name from "nebainfrastructure" to "nebamgmtinfrastructure".
    Modified container_name from "stage" to "terraform-state".
    Changed key from "nebamgmt.tfstate" to "stage.tfstate".
    
    These changes reflect a reorganization of Azure resources to align with new naming conventions and infrastructure requirements.

--- terraform/environments/stage/main.tf
+++ terraform/environments/stage/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "bowlneba"
-    storage_account_name = "nebainfrastructure"
-    container_name       = "stage"
-    key                  = "nebamgmt.tfstate"
+    resource_group_name  = "infrastructure"
+    storage_account_name = "nebamgmtinfrastructure"
+    container_name       = "terraform-state"
+    key                  = "stage.tfstate"
   }
 }
