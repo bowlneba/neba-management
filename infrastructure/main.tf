1b24c72cb98f5833b76043e0229dcae24b509549
David Kipperman <kippermand@gmail.com>
2024-09-20 17:36:00 -0400

    Refactor deployment workflow for Terraform and Azure: Update backend configuration key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name = var.backend_resource_group_name
     storage_account_name = var.backend_storage_account_name
     container_name = var.backend_container_name
-    key = "${terraform.workspace}.tfstate"
+    key = "${var.environment}.tfstate"
   }
 }
 
