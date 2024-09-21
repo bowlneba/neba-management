047950e4d2b86e64311ff0fb42dca85020928475
David Kipperman <kippermand@gmail.com>
2024-09-21 12:37:51 -0400

    Refactor deployment workflow: Revert commented out required_providers block in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  # required_providers {
-  #   azurerm = {
-  #     source  = "hashicorp/azurerm"
-  #     version = "~> 4.0"
-  #   }
-  # }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 
   backend "azurerm" {
     resource_group_name = ""
