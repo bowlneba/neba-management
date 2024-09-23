457e38409d7aff40c0d5da08cf146993c63a7b08
Dave Kipperman <kippermand@gmail.com>
2024-09-23 11:27:46 -0400

    Update budget threshold, SKU, and add tags to service plan
    Updated `resource_group_budget_threshold` to 10 in `terraform.tfvars`.
    Changed `app_service_plan_sku_name` from "Free" to "Shared" in `terraform.tfvars`.
    Added `tags` block to `azurerm_service_plan` in `main.tf` for "environment" and "owner".

--- infrastructure/modules/application/main.tf
+++ infrastructure/modules/application/main.tf
@@ -4,4 +4,9 @@ resource "azurerm_service_plan" "asp-nebamgmt" {
   resource_group_name = var.resource_group_name
   os_type = "Linux"
   sku_name = var.app_service_plan_sku_name
+
+  tags = {
+    "environment" = var.environment,
+    "owner" = var.owner
+  }
 }
