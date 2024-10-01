a8ede98442afd6f0b754df9668689044164cdc75
David Kipperman <kippermand@gmail.com>
2024-10-01 14:56:38 -0400

    Add slow query logging to NebaDbContext
    Updated `appsettings.json` to include `SlowQueryThresholdMilliseconds` setting. Modified `NebaDbContext` to accept `ILoggerFactory` and `IConfiguration` parameters, and added a slow query interceptor. Added `SlowQueryInterceptor.cs` to log warnings for slow queries.

--- src/Neba.Infrastructure/Persistence/NebaDbContext.cs
+++ src/Neba.Infrastructure/Persistence/NebaDbContext.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Neba.Infrastructure.Persistence.Interceptors;
 
 namespace Neba.Infrastructure.Persistence;
 
@@ -8,11 +12,30 @@ namespace Neba.Infrastructure.Persistence;
 public abstract class NebaDbContext
     : DbContext
 {
+    private readonly int _slowQueryThresholdInMilliseconds;
+    private readonly ILoggerFactory _loggerFactory;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="NebaDbContext"/> class.
     /// </summary>
     /// <param name="options">The options to be used by the DbContext.</param>
-    protected NebaDbContext(DbContextOptions options)
+    /// <param name="loggerFactory">The logger factory to create loggers.</param>
+    /// <param name="config">The configuration to retrieve settings.</param>
+    protected NebaDbContext(DbContextOptions options, ILoggerFactory loggerFactory, IConfiguration config)
         : base(options)
-    { }
+    {
+        _loggerFactory = loggerFactory;
+        _slowQueryThresholdInMilliseconds = config.GetValue<int>("Database:SlowQueryThresholdInMilliseconds");
+    }
+
+    /// <summary>
+    /// Configures the database context options.
+    /// </summary>
+    /// <param name="optionsBuilder">The options builder to configure.</param>
+    protected override void OnConfiguring([NotNull] DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(new SlowQueryInterceptor(_loggerFactory.CreateLogger<SlowQueryInterceptor>(), _slowQueryThresholdInMilliseconds));
+
+        base.OnConfiguring(optionsBuilder);
+    }
 }
