e1ca0bd25e26310d9a1f8c1da6ed0de7db5ad8bd
David Kipperman <kippermand@gmail.com>
2024-10-01 11:08:48 -0400

    Update package versions and add ICommand interface
    Updated `Directory.Packages.props` to include new versions for `ErrorOr` (2.0.1) and `MediatR` (12.4.1). Modified `Neba.Application.csproj` and `Neba.Domain.csproj` to add package references for `MediatR` and `ErrorOr` respectively. Created `ICommand.cs` to define `ICommand` and `ICommand<TResult>` interfaces, importing `ErrorOr` and `MediatR` namespaces, and introducing a marker interface `IBaseCommand`.

---
+++ src/Neba.Application/Messaging/ICommand.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Neba.Application.Messaging;
+
+/// <summary>
+/// Marker interface for base commands.
+/// </summary>
+public interface IBaseCommand;
+
+/// <summary>
+/// Represents a command that returns a success or error result.
+/// </summary>
+public interface ICommand
+    : IRequest<ErrorOr<Success>>, IBaseCommand;
+
+/// <summary>
+/// Represents a command that returns a result of type <typeparamref name="TResult"/> or an error.
+/// </summary>
+/// <typeparam name="TResult">The type of the result.</typeparam>
+public interface ICommand<TResult>
+    : IRequest<ErrorOr<TResult>>, IBaseCommand;
