3833bc23be3946eb304b51f46489c912323ba8ec
David Kipperman <kippermand@gmail.com>
2024-10-01 11:20:44 -0400

    Add ApplicationDependencyInjection for MediatR services
    Introduce a new static class `ApplicationDependencyInjection` in the
    `Neba.Application` namespace. This class provides an extension method
    `AddSharedApplicationServices` for the `IServiceCollection` interface.
    The method registers MediatR services from the assembly containing
    `ApplicationDependencyInjection` to the dependency injection container,
    facilitating the addition of shared application services.

---
+++ src/Neba.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Neba.Application;
+
+/// <summary>
+/// Provides extension methods for adding application services to the dependency injection container.
+/// </summary>
+public static class ApplicationDependencyInjection
+{
+    /// <summary>
+    /// Adds shared application services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to add the services to.</param>
+    /// <returns>The service collection with the added services.</returns>
+    public static IServiceCollection AddSharedApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(ApplicationDependencyInjection).Assembly);
+        });
+
+        return services;
+    }
+}
