52049d8afc0d14a4603e5b417fb20dff1e7df5ab
David Kipperman <kippermand@gmail.com>
2025-12-06 13:26:05 -0500

    Add ActiveFormat property to TournamentType
    Introduces the ActiveFormat boolean property to TournamentType to indicate whether a tournament type is an active format. Updates all static instances and unit tests to support and verify this new property.

--- src/backend/Neba.Domain/Tournaments/TournamentType.cs
+++ src/backend/Neba.Domain/Tournaments/TournamentType.cs
@@ -16,72 +16,72 @@ public sealed class TournamentType
     /// <summary>
     /// Singles tournament (1 player per team).
     /// </summary>
-    public static readonly TournamentType Singles = new("Singles", 10, 1);
+    public static readonly TournamentType Singles = new("Singles", 10, 1, true);
 
     /// <summary>
     /// Doubles tournament (2 players per team).
     /// </summary>
-    public static readonly TournamentType Doubles = new("Doubles", 20, 2);
+    public static readonly TournamentType Doubles = new("Doubles", 20, 2, true);
 
     /// <summary>
     /// Trios tournament (3 players per team).
     /// </summary>
-    public static readonly TournamentType Trios = new("Trios", 30, 3);
+    public static readonly TournamentType Trios = new("Trios", 30, 3, true);
 
     /// <summary>
     /// Non-Champions tournament.
     /// </summary>
-    public static readonly TournamentType NonChampions = new("Non-Champions", 11, 1);
+    public static readonly TournamentType NonChampions = new("Non-Champions", 11, 1, true);
 
     /// <summary>
     /// Tournament of Champions event.
     /// </summary>
-    public static readonly TournamentType TournamentOfChampions = new("Tournament of Champions", 12, 1);
+    public static readonly TournamentType TournamentOfChampions = new("Tournament of Champions", 12, 1, true);
 
     /// <summary>
     /// Invitational tournament.
     /// </summary>
-    public static readonly TournamentType Invitational = new("Invitational", 13, 1);
+    public static readonly TournamentType Invitational = new("Invitational", 13, 1, true);
 
     /// <summary>
     /// Masters tournament.
     /// </summary>
-    public static readonly TournamentType Masters = new("Masters", 14, 1);
+    public static readonly TournamentType Masters = new("Masters", 14, 1, true);
 
     /// <summary>
     /// High Roller tournament.
     /// </summary>
-    public static readonly TournamentType HighRoller = new("High Roller", 15, 1);
+    public static readonly TournamentType HighRoller = new("High Roller", 15, 1, false);
 
     /// <summary>
     /// Senior tournament.
     /// </summary>
-    public static readonly TournamentType Senior = new("Senior", 16, 1);
+    public static readonly TournamentType Senior = new("Senior", 16, 1, true);
 
     /// <summary>
     /// Women tournament.
     /// </summary>
-    public static readonly TournamentType Women = new("Women", 17, 1);
+    public static readonly TournamentType Women = new("Women", 17, 1, true);
 
     /// <summary>
     /// Over 40 tournament.
     /// </summary>
-    public static readonly TournamentType OverForty = new("Over 40", 18, 1);
+    public static readonly TournamentType OverForty = new("Over 40", 18, 1, false);
 
     /// <summary>
     /// 40-49 age group tournament.
     /// </summary>
-    public static readonly TournamentType FortyToFortyNine = new("40 - 49", 19, 1);
+    public static readonly TournamentType FortyToFortyNine = new("40 - 49", 19, 1, false);
 
     /// <summary>
     /// Over/Under 50 Doubles tournament (2 players per team).
     /// </summary>
-    public static readonly TournamentType OverUnderFiftyDoubles = new("Over/Under 50 Doubles", 21, 2);
+    public static readonly TournamentType OverUnderFiftyDoubles = new("Over/Under 50 Doubles", 21, 2, true);
 
     /// <summary>
     /// Over/Under 40 Doubles tournament (2 players per team).
     /// </summary>
-    public static readonly TournamentType OverUnderFortyDoubles = new("Over/Under 40 Doubles", 22, 2);
+    public static readonly TournamentType OverUnderFortyDoubles = new("Over/Under 40 Doubles", 22, 2, false);
 
     /// <summary>
     /// Initializes a new instance of the <see cref="TournamentType"/> class.
@@ -89,10 +89,12 @@ public sealed class TournamentType
     /// <param name="name">The display name of the tournament type.</param>
     /// <param name="value">The unique integer value for the tournament type.</param>
     /// <param name="teamSize">The number of players per team for this tournament type.</param>
-    private TournamentType(string name, int value, int teamSize)
+    /// <param name="activeFormat">Indicates whether this tournament type is an active format.</param>
+    private TournamentType(string name, int value, int teamSize, bool activeFormat)
         : base(name, value)
     {
         TeamSize = teamSize;
+        ActiveFormat = activeFormat;
     }
 
     /// <summary>
@@ -106,4 +108,10 @@ public sealed class TournamentType
     /// Gets the number of players per team for this tournament type.
     /// </summary>
     public int TeamSize { get; }
+
+    /// <summary>
+    /// Indicates whether this tournament type is an active format.
+    /// </summary>
+    /// <value>True if the tournament type is an active format; otherwise, false.</value>
+    public bool ActiveFormat { get; }
 }
