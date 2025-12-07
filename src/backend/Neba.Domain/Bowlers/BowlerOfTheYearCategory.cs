476e435b81e1754dc968387e1c799e438d6ea774
David Kipperman <kippermand@gmail.com>
2025-12-07 15:10:02 -0500

    Add BowlerOfTheYearCategory smart enum and tests
    Introduces BowlerOfTheYearCategory as a smart enum to represent award categories in the domain model. Adds unit tests to verify the correct categories, names, and values.

---
+++ src/backend/Neba.Domain/Bowlers/BowlerOfTheYearCategory.cs
@@ -0,0 +1,56 @@
+using Ardalis.SmartEnum;
+
+namespace Neba.Domain.Bowlers;
+
+/// <summary>
+/// Represents a category for the Bowler of the Year award, using a smart enum pattern.
+/// </summary>
+public sealed class BowlerOfTheYearCategory
+    : SmartEnum<BowlerOfTheYearCategory>
+{
+    /// <summary>
+    /// Open category for Bowler of the Year.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory Open = new("Bowler of the Year", 1);
+
+    /// <summary>
+    /// Woman category for Bowler of the Year.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory Woman = new("Woman Bowler of the Year", 2);
+
+    /// <summary>
+    /// Senior category for Bowler of the Year.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory Senior = new("Senior Bowler of the Year", 50);
+
+    /// <summary>
+    /// Super Senior category for Bowler of the Year.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory SuperSenior = new("Super Senior Bowler of the Year", 60);
+
+    /// <summary>
+    /// Rookie of the Year category.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory Rookie = new("Rookie of the Year", 10);
+
+    /// <summary>
+    /// Youth category for Bowler of the Year.
+    /// </summary>
+    public static readonly BowlerOfTheYearCategory Youth = new("Youth Bowler of the Year", 20);
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BowlerOfTheYearCategory"/> class with the specified name and value.
+    /// </summary>
+    /// <param name="name">The name of the category.</param>
+    /// <param name="value">The integer value of the category.</param>
+    private BowlerOfTheYearCategory(string name, int value)
+        : base(name, value)
+    { }
+
+    /// <summary>
+    /// Private parameterless constructor for serialization or ORM support.
+    /// </summary>
+    private BowlerOfTheYearCategory()
+        : this(string.Empty, 0)
+    {}
+}
---
+++ tests/Neba.UnitTests/Bowlers/BowlerOfTheYearCategoryTests.cs
@@ -0,0 +1,35 @@
+using Neba.Domain.Bowlers;
+
+namespace Neba.UnitTests.Bowlers;
+
+public sealed class BowlerOfTheYearCategoryTests
+{
+    [Fact]
+    public void BowlerOfTheYearCategory_ShouldHaveSixCategories()
+    {
+        // Arrange & Act
+        IReadOnlyCollection<BowlerOfTheYearCategory> categories = Neba.Domain.Bowlers.BowlerOfTheYearCategory.List;
+
+        // Assert
+        categories.Count.ShouldBe(6);
+    }
+
+    [Theory]
+    [InlineData("Bowler of the Year", 1)]
+    [InlineData("Woman Bowler of the Year", 2)]
+    [InlineData("Senior Bowler of the Year", 50)]
+    [InlineData("Super Senior Bowler of the Year", 60)]
+    [InlineData("Rookie of the Year", 10)]
+    [InlineData("Youth Bowler of the Year", 20)]
+    public void BowlerOfTheYearCategory_ShouldHaveCorrectNameAndValue(string expectedName, int expectedValue)
+    {
+        // Arrange & Act
+        BowlerOfTheYearCategory? category = BowlerOfTheYearCategory.List
+            .SingleOrDefault(c => c.Name == expectedName && c.Value == expectedValue);
+
+        // Assert
+        category.ShouldNotBeNull();
+        category.Name.ShouldBe(expectedName);
+        category.Value.ShouldBe(expectedValue);
+    }
+}
