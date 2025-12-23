a3a3e3e0a1585d3cc9ae3c40bac3ec92dd40a541
David Kipperman <kippermand@gmail.com>
2025-12-23 14:58:54 -0500

    Suppress additional SonarAnalyzer warnings in codebase
    Added pragma directives to disable S1144 and S2326 SonarAnalyzer warnings in OpenApiExtensions.cs and IQuery.cs, respectively. This helps prevent unnecessary warnings for unused constructors and type parameters in internal or generic types.

--- src/backend/Neba.Application/Messaging/IQuery.cs
+++ src/backend/Neba.Application/Messaging/IQuery.cs
@@ -1,5 +1,7 @@
 namespace Neba.Application.Messaging;
 
+#pragma warning disable S2326 // Unused type parameters should be removed
+
 /// <summary>
 /// Represents a query that returns a response of type <typeparamref name="TResponse"/>.
 /// Used in the CQRS pattern to encapsulate a request for data.
