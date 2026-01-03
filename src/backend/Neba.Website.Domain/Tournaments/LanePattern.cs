c37b3a432cb2d4f1e5cac0546aa3b6582f64432b
David Kipperman <kippermand@gmail.com>
2026-01-03 11:35:59 -0500

    Add JSON converters for LanePattern enum properties
    Applied SmartEnumValueConverter attributes to LengthCategory and RatioCategory properties in LanePattern to enable correct serialization and deserialization with System.Text.Json.

--- src/backend/Neba.Website.Domain/Tournaments/LanePattern.cs
+++ src/backend/Neba.Website.Domain/Tournaments/LanePattern.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+using Ardalis.SmartEnum.SystemTextJson;
 using Neba.Domain.Tournaments;
 
 namespace Neba.Website.Domain.Tournaments;
@@ -10,10 +12,12 @@ public sealed record LanePattern
     /// <summary>
     /// Gets the length of the lane pattern.
     /// </summary>
+    [JsonConverter(typeof(SmartEnumValueConverter<PatternLengthCategory, int>))]
     public required PatternLengthCategory LengthCategory { get; init; }
 
     /// <summary>
     /// Gets the ratio of the lane pattern.
     /// </summary>
+    [JsonConverter(typeof(SmartEnumValueConverter<PatternRatioCategory, int>))]
     public required PatternRatioCategory RatioCategory { get; init; }
 }
