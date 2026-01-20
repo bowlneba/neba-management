e5c8cb984cef66de29866b1efd5f7b2697ac768b
David Kipperman <kippermand@gmail.com>
2026-01-20 16:42:36 -0500

    Rename TournamentDocumentTypeTests to TournamentFileTypeTests
    Replaces the TournamentDocumentTypeTests with TournamentFileTypeTests to reflect a change in the tested domain type. The new test class verifies the properties and count of TournamentFileType instead of TournamentDocumentType.

---
+++ tests/Neba.UnitTests/Website/Tournaments/TournamentFileTypeTests.cs
@@ -0,0 +1,31 @@
+﻿
+using Neba.Website.Domain.Tournaments;
+
+namespace Neba.UnitTests.Website.Tournaments;
+
+public sealed class TournamentFileTypeTests
+{
+    [Fact(DisplayName = "TournamentFileType should have one file type")]
+    public void TournamentFileType_ShouldHaveOneFileType()
+    {
+        // Arrange & Act
+        IReadOnlyCollection<TournamentFileType> fileTypes = TournamentFileType.List;
+
+        // Assert
+        fileTypes.Count.ShouldBe(1);
+    }
+
+    [Theory(DisplayName = "Has correct name and value for all file types")]
+    [InlineData("Logo", 1, TestDisplayName = "Logo file type is correct")]
+    public void TournamentFileType_ShouldHaveCorrectNameAndValue(string expectedName, int expectedValue)
+    {
+        // Arrange & Act
+        TournamentFileType? fileType = TournamentFileType.List
+            .SingleOrDefault(dt => dt.Name == expectedName);
+
+        // Assert
+        fileType.ShouldNotBeNull();
+        fileType.Name.ShouldBe(expectedName);
+        fileType.Value.ShouldBe(expectedValue);
+    }
+}
