88ae9e25117fae4deb575dcabbca460a6ca9f25d
David Kipperman <kippermand@gmail.com>
2026-01-20 16:21:19 -0500

    Add TournamentFile entity and migration
    Introduces the TournamentFile entity and related database table to support storing files associated with tournaments. Updates the EF Core model snapshot and adds migration for table creation, including foreign key to tournaments and relevant indexes.

--- src/backend/Neba.Website.Infrastructure/Database/Migrations/20260120212035_TournamentFiles.Designer.cs
+++ src/backend/Neba.Website.Infrastructure/Database/Migrations/20260120212035_TournamentFiles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Neba.Website.Infrastructure.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Neba.Website.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(WebsiteDbContext))]
-    partial class WebsiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260120212035_TournamentFiles")]
+    partial class TournamentFiles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
